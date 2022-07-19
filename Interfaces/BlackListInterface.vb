28f150769b844fd01f9a61848ac5f9a52f4b040f
Theo Verweij <tverweij@xs4all.nl>
2022-07-19 18:31:40 +0200

    Refactored
    Refactored to make better layer seperation, added interfaces to be able to implement for other firewalls and blacklists

---
+++ Interfaces/BlackListInterface.vb
@@ -0,0 +1,20 @@
+Public Interface BlackListInterface
+
+  //The API key to use for this blacklist
+  Property API_Key As String
+
+  //Checks an IP Address, returns true when the IP address is safe (false for blocked)
+  //The implementation should persist all lookups which can be read again with the ReadLookupCache sub
+  Function CheckIP(IP As String) As Boolean
+
+  //Reads a predefined blacklist with blocked addresses that will be cached
+  //The implementation should do an online download of the blacklist every 12 hours, and should cache this blacklist.
+  //When the software is restarted, it should only read a new blacklist when the saved one is older than 12 hours
+  //When reading the online or cached blacklist, those addresses should be cached.
+  Sub ReadBlacklist
+
+  //Reads the persisted cache
+  Sub ReadLookupCache
+
+
+End Interface
